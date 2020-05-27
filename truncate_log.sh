e3938b8166f4824cb27d8bd622fdbef13aee6c75
Tien Phan <tien.phanthe@gmail.com>
2020-05-27 14:37:28 +0700

    Update truncate_log.sh

--- truncate_log.sh
+++ truncate_log.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Before deleting the logs, if you have a huge one, a good practice is to keep the last 100 lines or so, and then delete it. 
+# So, you can check what is happening after generating free space.
+# If you just delete it with “rm” and the log file is being used by the PostgreSQL server (or another service) space won’t 
+# be released, so you should truncate this file using this cat /dev/null command instead.
+
 tail -100 /var/log/postgresql/postpostgresql-Fri.log > /tmp/log_temp.log
 cat /dev/null > /var/log/postgresql/postpostgresql-Fri.log
