993f7c51173a44e310058b87aa01a765ffeacc09
Tien Phan <tien.phanthe@gmail.com>
2020-06-04 14:01:11 +0700

    Update docker_clean_overlay2_dir.sh

--- docker_clean_overlay2_dir.sh
+++ docker_clean_overlay2_dir.sh
@@ -11,3 +11,12 @@
 
 # docker remove exited docker
 /usr/bin/docker rm -v $(docker ps -a -q -f status=exited)
+
+#### CRON JOB ########################################################################
+# docker system prune
+#* 3 * * * /usr/bin/docker system prune -f > /dev/null
+# docker remove exited docker
+#30 * * * * /usr/bin/docker rm -v $(docker ps -a -q -f status=exited)
+# docker volume rm
+#0 */6 * * * /usr/bin/docker volume rm -f $(docker volume ls -q -f dangling=true)
+######################################################################################
