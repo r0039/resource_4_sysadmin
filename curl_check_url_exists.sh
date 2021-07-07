367b9741791fb52e42d8e1e06ec9515d906e7051
Tien Phan <tien.phanthe@gmail.com>
2021-07-07 13:51:44 +0700

    Create curl_check_url_exists.sh

---
+++ curl_check_url_exists.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# --fail will make the exit status nonzero on a failed request
+# --head will avoid downloading the file contents, since we don't need it for this check
+# --silent will avoid status or errors from being emitted by the check itself.
+#
+# If your server refuses HEAD requests, an alternative is to request only the first byte of the file
+# if curl --output /dev/null --silent --fail -r 0-0 "$url"; then
+#
+# if wget --spider http://google.com 2>/dev/null; then
+#  echo "File exists"
+# else
+#  echo "File does not exist"
+# fi
+#
+# --spider option makes wget just check for the file instead of downloading it
+# 2> /dev/null silences wget's stderr output
+#
+
+if curl --output /dev/null --silent --head --fail "$url"; then
+  echo "URL exists: $url"
+else
+  echo "URL does not exist: $url"
+fi
