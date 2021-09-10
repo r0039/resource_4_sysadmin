42337ad5d629aac382b56b1ffb7510dab2f2d474
Tien Phan <tien.phanthe@gmail.com>
2021-09-10 09:19:40 +0700

    Update find_inode.sh

--- find_inode.sh
+++ find_inode.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+# https://unix.stackexchange.com/questions/130899/finding-the-maximum-of-the-values-in-a-file 
 
 # list all of inode 
 find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n
