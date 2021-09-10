8a3a48106ba12881c746a84b744b1934452c6e4f
Tien Phan <tien.phanthe@gmail.com>
2021-09-10 08:54:32 +0700

    Update find_inode.sh

--- find_inode.sh
+++ find_inode.sh
@@ -1,3 +1,7 @@
 #!/bin/bash 
 
+# list all of inode 
 find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n
+
+# list the top of inode 
+find . -xdev -type f | cut -d "/" -f 2 | sort | uniq -c | sort -n | tail -1
