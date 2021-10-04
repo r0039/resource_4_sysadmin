60aa9255e4544024f94df5b04ef1cff6ecfcdcf2
Tien Phan <tien.phanthe@gmail.com>
2021-10-04 17:52:02 +0700

    Update generate_gpg_4096.sh

--- bashscript/generate_gpg_4096.sh
+++ bashscript/generate_gpg_4096.sh
@@ -9,3 +9,6 @@ gpg --list-secret-keys --keyid-format=long
 
 # export GPG key
 gpg --armor --export 3A75F63E1FCF379A0A0E762FF1BD4862F2A
+
+# export GPG key to file 
+gpg --armor --output tien.phan.gpg --export 3A75F63E1FCF379A0A0E762FF1BD4862F2A
