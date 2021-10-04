a4cc4cd20a5f067053788ef7441b391e1b671c9e
Tien Phan <tien.phanthe@gmail.com>
2021-10-04 17:49:01 +0700

    Create generate_gpg_4096.sh

---
+++ bashscript/generate_gpg_4096.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+# source https://docs.github.com/en/authentication/managing-commit-signature-verification/generating-a-new-gpg-key 
+
+# create GPG RSA 4096
+gpg --default-new-key-algo rsa4096 --gen-key
+
+# list GPG keys 
+gpg --list-secret-keys --keyid-format=long
+
+# export GPG key
+gpg --armor --export 3A75F63E1FCF379A0A0E762FF1BD4862F2A
